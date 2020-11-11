d04c23c721386a4f1fbd1f194aee148323fcd086
tranbathanhtung <tranbathanhtung222@gmail.com>
2020-11-11 10:36:50 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import "candy-moon/dist/reset.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
